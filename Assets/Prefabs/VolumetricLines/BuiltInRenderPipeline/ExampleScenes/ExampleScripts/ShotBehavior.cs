2fb2138823b4f6787aac9e4bd5c0ce6be98463a4
artyom <goastgavrish444@gmail.com>
2021-03-31 20:53:47 +0300

    Add starship
    Add Asteroids
    Add controllers

---
+++ Assets/Prefabs/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+
+	public float speed;
+
+
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * speed;
+	}
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+
+
+}
